540a325ba4d32376ba586af04c137154a855b5db
trandrew08 <riantuter@gmail.com>
2016-09-19 10:49:04 -0400

    Small mods on jQuery and CSS. Added message box after data submission.

--- javascript/driver.js
+++ javascript/driver.js
@@ -3,15 +3,38 @@ jQuery(document).ready(function($){
 		$('#SignInBox').toggle(200);
 		$('#headliner').show(200);
 		$('#SignUpForm').hide(200);
+		$('#SignUpSuccessMsg').hide();
+		($).history.pushState();
 	});
 	$('#Sign_Up').click(function(){
 		$('#SignUpForm').show(200);
 		$('#headliner').hide(200);
 		$('#SignInBox').hide(200);
+		$('#SignUpSuccessMsg').hide();
+		($).history.pushState();
 	});
 	$('#cancelButton').click(function(){
 		$('#SignUpForm').hide(200);
 		$('#headliner').show(200);
+		$('#SignUpSuccessMsg').hide();
+		($).history.pushState();
+	});
+	$('#Sign_Up_Submit').click(function(){
+		$('#SignUpForm').hide(100);
+		$('#SignUpSuccessMsg').show(200);
+		($).history.pushState();
 	});
 });
+
+jQuery(document).ready(function(){
+	$.bind('back',fuction(){
+		historyBack();
+	});
+});
+
+function historyBack(){
+	window.history.pop();
+}
+
+
 
